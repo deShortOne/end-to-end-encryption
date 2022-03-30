653195fa6fff945b1f23e5b0a94d1b25c8340641
deShortOne <jingshianggu@gmail.com>
2022-03-30 22:48:29 +0100

    First commit

---
+++ src/main/java/com/github/deShortOne/peer_to_peer_encryption/Main.java
@@ -0,0 +1,12 @@
+package com.github.deShortOne.peer_to_peer_encryption;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/com/github/deShortOne/peer_to_peer_encryption/AppTest.java
@@ -0,0 +1,12 @@
+package com.github.deShortOne.peer_to_peer_encryption;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
