c2f4a2cec26c07b867642afddb3e981e309fe20c
deShortOne <jingshianggu@gmail.com>
2022-03-31 11:12:10 +0100

    Copied in file encryption and decryption

--- src/main/java/com/github/deShortOne/peer_to_peer_encryption/AESEncryption.java
+++ src/main/java/com/github/deShortOne/peer_to_peer_encryption/AESEncryption.java
@@ -1,5 +1,9 @@
 package com.github.deShortOne.peer_to_peer_encryption;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -34,6 +38,20 @@ public class AESEncryption {
 		System.out.println("Hello World!");
 	}
 
+	/**
+	 * String encryption.
+	 * @param algorithm
+	 * @param input
+	 * @param key
+	 * @param iv
+	 * @return
+	 * @throws NoSuchPaddingException
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidAlgorithmParameterException
+	 * @throws InvalidKeyException
+	 * @throws BadPaddingException
+	 * @throws IllegalBlockSizeException
+	 */
 	public static String encrypt(String algorithm, String input, SecretKey key,
 			IvParameterSpec iv)
 			throws NoSuchPaddingException, NoSuchAlgorithmException,
@@ -46,6 +64,20 @@ public class AESEncryption {
 		return Base64.getEncoder().encodeToString(cipherText);
 	}
 
+	/** 
+	 * String decryption.
+	 * @param algorithm
+	 * @param cipherText
+	 * @param key
+	 * @param iv
+	 * @return
+	 * @throws NoSuchPaddingException
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidAlgorithmParameterException
+	 * @throws InvalidKeyException
+	 * @throws BadPaddingException
+	 * @throws IllegalBlockSizeException
+	 */
 	public static String decrypt(String algorithm, String cipherText,
 			SecretKey key, IvParameterSpec iv)
 			throws NoSuchPaddingException, NoSuchAlgorithmException,
@@ -58,6 +90,57 @@ public class AESEncryption {
 				.doFinal(Base64.getDecoder().decode(cipherText));
 		return new String(plainText);
 	}
+	
+	// encryption and decryption of file is basically the same
+	public static void encryptFile(String algorithm, SecretKey key, IvParameterSpec iv,
+		    File inputFile, File outputFile) throws IOException, NoSuchPaddingException,
+		    NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+		    BadPaddingException, IllegalBlockSizeException {
+		    
+		    Cipher cipher = Cipher.getInstance(algorithm);
+		    cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+		    FileInputStream inputStream = new FileInputStream(inputFile);
+		    FileOutputStream outputStream = new FileOutputStream(outputFile);
+		    byte[] buffer = new byte[64];
+		    int bytesRead;
+		    while ((bytesRead = inputStream.read(buffer)) != -1) {
+		        byte[] output = cipher.update(buffer, 0, bytesRead);
+		        if (output != null) {
+		            outputStream.write(output);
+		        }
+		    }
+		    byte[] outputBytes = cipher.doFinal();
+		    if (outputBytes != null) {
+		        outputStream.write(outputBytes);
+		    }
+		    inputStream.close();
+		    outputStream.close();
+		}
+	
+	public static void decryptFile(String algorithm, SecretKey key, IvParameterSpec iv,
+		    File inputFile, File outputFile) throws IOException, NoSuchPaddingException,
+		    NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+		    BadPaddingException, IllegalBlockSizeException {
+		    
+		    Cipher cipher = Cipher.getInstance(algorithm);
+		    cipher.init(Cipher.DECRYPT_MODE, key, iv);
+		    FileInputStream inputStream = new FileInputStream(inputFile);
+		    FileOutputStream outputStream = new FileOutputStream(outputFile);
+		    byte[] buffer = new byte[64];
+		    int bytesRead;
+		    while ((bytesRead = inputStream.read(buffer)) != -1) {
+		        byte[] output = cipher.update(buffer, 0, bytesRead);
+		        if (output != null) {
+		            outputStream.write(output);
+		        }
+		    }
+		    byte[] outputBytes = cipher.doFinal();
+		    if (outputBytes != null) {
+		        outputStream.write(outputBytes);
+		    }
+		    inputStream.close();
+		    outputStream.close();
+		}
 
 	// https://docs.oracle.com/en/java/javase/11/docs/api/java.base/javax/crypto/KeyGenerator.html
 	// n is 128, 192 or 256 for AES
--- src/test/java/com/github/deShortOne/peer_to_peer_encryption/AppTest.java
+++ src/test/java/com/github/deShortOne/peer_to_peer_encryption/AppTest.java
@@ -1,8 +1,11 @@
 package com.github.deShortOne.peer_to_peer_encryption;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
@@ -13,7 +16,11 @@ import javax.crypto.spec.IvParameterSpec;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AppTest {
+
+	static String fileLocation = "dummy_files\\";
 
 	@Test
 	void givenString_whenEncrypt_thenSuccess() throws NoSuchAlgorithmException,
@@ -29,5 +36,46 @@ public class AppTest {
 		String plainText = AESEncryption.decrypt(algorithm, cipherText, key,
 				ivParameterSpec);
 		Assertions.assertEquals(input, plainText);
+	}
+
+	@Test
+	void givenFile_whenEncrypt_thenSuccess()
+			throws NoSuchAlgorithmException, IOException,
+			IllegalBlockSizeException, InvalidKeyException, BadPaddingException,
+			InvalidAlgorithmParameterException, NoSuchPaddingException {
+
+		SecretKey key = AESEncryption.generateKey(128);
+		String algorithm = "AES/CBC/PKCS5Padding";
+		IvParameterSpec ivParameterSpec = AESEncryption.generateIv();
+		File inputFile = new File(fileLocation + "dummy.txt");
+		File encryptedFile = new File(fileLocation + "document.encrypted");
+		File decryptedFile = new File(fileLocation + "document.decrypted");
+		AESEncryption.encryptFile(algorithm, key, ivParameterSpec, inputFile,
+				encryptedFile);
+		AESEncryption.decryptFile(algorithm, key, ivParameterSpec,
+				encryptedFile, decryptedFile);
+		assertThat(inputFile).hasSameTextualContentAs(decryptedFile);
+	}
+
+	@Test
+	void givenPassword_whenEncrypt_thenSuccess()
+			throws InvalidKeySpecException, NoSuchAlgorithmException,
+			IllegalBlockSizeException, InvalidKeyException, BadPaddingException,
+			InvalidAlgorithmParameterException, NoSuchPaddingException {
+
+		String algorithm = "AES/CBC/PKCS5Padding";
+		String plainText = "www.baeldung.com";
+		String password = "baeldung";
+		String salt = "12345678";
+		IvParameterSpec ivParameterSpec = AESEncryption.generateIv();
+		SecretKey key = AESEncryption.getKeyFromPassword(password, salt);
+		String cipherText = AESEncryption.encrypt(algorithm, plainText, key,
+				ivParameterSpec);
+		String decryptedCipherText = AESEncryption.decrypt(algorithm,
+				cipherText, key, ivParameterSpec);
+		Assertions.assertEquals(plainText, decryptedCipherText);
+
+		// Should be calling below function
+		// AESEncryotion.encryptPasswordBased(plainText, key, ivParameterSpec);
 	}
 }
