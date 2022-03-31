9df49ad677ba2b4d6313c9e11fed7a04e1588c66
deShortOne <jingshianggu@gmail.com>
2022-03-31 22:47:45 +0100

    CSV for signing in and logging in

--- src/main/java/com/github/deShortOne/peer_to_peer_encryption/StartScreen.java
+++ src/main/java/com/github/deShortOne/peer_to_peer_encryption/StartScreen.java
@@ -1,5 +1,17 @@
 package com.github.deShortOne.peer_to_peer_encryption;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
+
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
@@ -15,45 +27,176 @@ import javafx.scene.text.Text;
  */
 public class StartScreen {
 
+	public static String fileLoc = "files\\accounts.csv";
+
 	public static Parent loginPage() {
 		GridPane grid = new GridPane();
 		grid.setHgap(10);
-	    grid.setVgap(10);
-	    grid.setPadding(new Insets(0, 10, 0, 10));
-	    
-	    Text signInMode = new Text("Log in");
-	    signInMode.setId("SignInModeText");
-	    signInMode.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-	    grid.add(signInMode, 0, 0);
-	    
-	    Text usernameText = new Text("Username");
-	    grid.add(usernameText, 0, 1, 2, 1);
-	    
-	    TextField username = new TextField("Username");
-	    grid.setId("usernameInput");
-	    grid.add(username, 0, 2);
-	    
-	    Text passwordText = new Text("Password");
-	    grid.add(passwordText, 0, 3, 2, 1);
-	    
-	    TextField passwordInput = new TextField("Password");
-	    grid.setId("Password");
-	    grid.add(passwordInput, 0, 4);
-	    
-	    Button signIn = new Button("Log in");
-	    signIn.setId("LoginButton");
-	    signIn.setOnAction(e -> {
-	    	String s;
-	    	if (signIn.getText().equals("Log in")) {
-	    		s = "Sign in";
-	    	} else {
-	    		s = "Log in";
-	    	}
-	    	signInMode.setText(s);
-    		signIn.setText(s);
-	    });
-	    grid.add(signIn, 0, 5);
-	    
-	    return grid;
+		grid.setVgap(10);
+		grid.setPadding(new Insets(0, 10, 0, 10));
+
+		Text signInMode = new Text("Log in");
+		signInMode.setId("SignInModeText");
+		signInMode.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		grid.add(signInMode, 0, 0);
+
+		Text usernameText = new Text("Username");
+		grid.add(usernameText, 0, 1, 2, 1);
+
+		TextField username = new TextField("Username");
+		grid.setId("usernameInput");
+		grid.add(username, 0, 2);
+
+		Text passwordText = new Text("Password");
+		grid.add(passwordText, 0, 3, 2, 1);
+
+		TextField passwordInput = new TextField("Password");
+		grid.setId("Password");
+		grid.add(passwordInput, 0, 4);
+
+		Button signIn = new Button("Log in");
+		signIn.setId("LoginButton");
+		signIn.setOnAction(e -> {
+			String s;
+			if (signIn.getText().equals("Log in")) {
+				s = "Sign in";
+			} else {
+				s = "Log in";
+			}
+			signInMode.setText(s);
+			signIn.setText(s);
+		});
+		grid.add(signIn, 0, 5);
+
+		return grid;
+	}
+
+	public static boolean loginUsernameAndPassword(String username,
+			String password) {
+
+		String hashPassword = calculatePasswordHash(username, password);
+		if (hashPassword == null)
+			return false;
+
+		CSVReader reader;
+		try {
+			reader = new CSVReader(new FileReader(fileLoc));
+		} catch (FileNotFoundException e) {
+			// no password file = no saved accounts
+			e.printStackTrace();
+			return false;
+		}
+
+		String hashUsername = calculateUsernameHash(username);
+		String[] nextLine;
+		try {
+			while ((nextLine = reader.readNext()) != null) {
+				if (nextLine[0].equals(hashUsername)) {
+					return nextLine[1].equals(hashPassword);
+				}
+			}
+		} catch (CsvValidationException | IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static boolean signupUsernameAndPassword(String username,
+			String password) {
+
+		String hashUsername = calculateUsernameHash(username);
+
+		if (checkForDuplicate(hashUsername))
+			return false;
+
+		String hashPassword = calculatePasswordHash(username, password);
+		if (hashPassword == null)
+			return false;
+
+		FileWriter mFileWriter;
+		try {
+			mFileWriter = new FileWriter(fileLoc, true);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return false;
+		}
+
+		CSVWriter writer = new CSVWriter(mFileWriter);
+		writer.writeNext(new String[] { hashUsername, hashPassword });
+		try {
+			writer.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	private static boolean checkForDuplicate(String username) {
+		CSVReader reader;
+		try {
+			reader = new CSVReader(new FileReader(fileLoc));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		String[] nextLine;
+		try {
+			while ((nextLine = reader.readNext()) != null) {
+				if (nextLine[0].equals(username)) {
+					return true;
+				}
+			}
+		} catch (CsvValidationException | IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static String calculateUsernameHash(String username) {
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+		byte[] encodedhash = digest
+				.digest(username.getBytes(StandardCharsets.UTF_8));
+		return cleanHash(new String(encodedhash, StandardCharsets.UTF_8));
+	}
+
+	private static String calculatePasswordHash(String username,
+			String password) {
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+		byte[] encodedhash = digest
+				.digest(password.getBytes(StandardCharsets.UTF_8));
+
+		String tmp1 = new String(encodedhash, StandardCharsets.UTF_8)
+				+ username;
+
+		byte[] encodedhash1 = digest
+				.digest(tmp1.getBytes(StandardCharsets.UTF_8));
+		return cleanHash(new String(encodedhash1, StandardCharsets.UTF_8));
+	}
+
+	private static String cleanHash(String hash) {
+		StringBuilder sb = new StringBuilder();
+		for (char c : hash.toCharArray()) {
+			if (c == '\\') {
+
+			} else {
+				sb.append(c);
+			}
+		}
+
+		return sb.toString();
 	}
 }
--- src/test/java/com/github/deShortOne/peer_to_peer_encryption/StartScreenTest.java
+++ src/test/java/com/github/deShortOne/peer_to_peer_encryption/StartScreenTest.java
@@ -1,5 +1,8 @@
 package com.github.deShortOne.peer_to_peer_encryption;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +13,8 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.control.LabeledMatchers;
 
+import com.opencsv.CSVWriter;
+
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -19,26 +24,26 @@ import javafx.stage.Stage;
 
 @ExtendWith(ApplicationExtension.class)
 public class StartScreenTest {
-	
+
 	private Parent nodeToScene;
 
 	@Start
-    private void start(Stage stage) {
+	public void start(Stage stage) {
 		nodeToScene = StartScreen.loginPage();
 		Scene s = new Scene(nodeToScene);
-        stage.setScene(s);
-        stage.show();
-    }
+		stage.setScene(s);
+		stage.show();
+	}
 
 	@Test
-    public void buttonsAreCorrectAndSwitching(FxRobot robot) {
+	public void buttonsAreCorrectAndSwitching(FxRobot robot) {
 		Text signInText = null;
 		Button signIn = null;
-		
+
 		for (Node p : nodeToScene.getChildrenUnmodifiable()) {
 			if (p.getId() == null)
 				continue;
-			
+
 			if (p.getId().equals("LoginButton")) {
 				signIn = (Button) p;
 			} else if (p.getId().equals("SignInModeText")) {
@@ -47,16 +52,45 @@ public class StartScreenTest {
 		}
 		Assertions.assertNotEquals(null, signInText);
 		Assertions.assertNotEquals(null, signIn);
-		
+
 		Assertions.assertEquals("Log in", signInText.getText());
 		Assertions.assertEquals("Log in", signIn.getText());
-        
-		robot.clickOn(signIn);		
+
+		robot.clickOn(signIn);
 		Assertions.assertEquals("Sign in", signInText.getText());
 		Assertions.assertEquals("Sign in", signIn.getText());
-		
+
 		robot.clickOn(signIn);
 		Assertions.assertEquals("Log in", signInText.getText());
 		Assertions.assertEquals("Log in", signIn.getText());
+	}
+
+	@Test
+	public void checkUsernameAndPassword() throws IOException {
+		// Clears csv file
+		String csv = StartScreen.fileLoc;
+		CSVWriter writer = new CSVWriter(new FileWriter(csv));
+		writer.writeNext(null);
+		writer.close();
+
+		String username = "hiddenUser1";
+		String password = "lowbattery";
+
+		// No user with this username exist so login fails
+		Assertions.assertFalse(
+				StartScreen.loginUsernameAndPassword(username, password));
+		// No user with this username exists so sign up succeeds
+		Assertions.assertTrue(
+				StartScreen.signupUsernameAndPassword(username, password));
+		// Username with correct password so log in succeeds
+		Assertions.assertTrue(
+				StartScreen.loginUsernameAndPassword(username, password));
+
+		// Username with incorrect password so login fails
+		Assertions.assertFalse(
+				StartScreen.loginUsernameAndPassword(username, password + "a"));
+		// Already exists username with this username so sign up fails
+		Assertions.assertFalse(
+				StartScreen.signupUsernameAndPassword(username, password));
 	}
 }
